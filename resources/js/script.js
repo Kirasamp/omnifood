34fb5032102a5820005f435f583e3b87981ad8d1
Kimberley Sampson-Collett <kirasamp@gmail.com>
2021-07-16 14:30:20 +0200

    sticky nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,13 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  var waypoint = new Waypoint({
+    element: $(".js--section-features"),
+    handler: function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky-nav");
+      } else {
+        $("nav").removeClass("sticky-nav");
+      }
+    },
+    offset: "60px;",
+  });
+});
